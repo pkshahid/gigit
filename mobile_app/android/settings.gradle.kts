747897a2f6bf22fc2de5b1328c4fdfd30b20ac5d
ntoric <ntoric.com@gmail.com>
2026-06-30 17:28:39 +0530

    Update

--- mobile_app/android/settings.gradle.kts
+++ mobile_app/android/settings.gradle.kts
@@ -1,7 +1,13 @@
 pluginManagement {
-    val flutterVersionPath = settings.extra["flutter.sdk"] as String
-    includeBuild("$flutterVersionPath/packages/flutter_tools/gradle")
-    
+    val flutterSdkPath = run {
+        val properties = java.util.Properties()
+        file("local.properties").inputStream().use { properties.load(it) }
+        val flutterSdkPath = properties.getProperty("flutter.sdk")
+        require(flutterSdkPath != null) { "flutter.sdk not set in local.properties" }
+        flutterSdkPath
+    }
+    includeBuild("$flutterSdkPath/packages/flutter_tools/gradle")
+
     repositories {
         google()
         mavenCentral()
