89b8841f1804cb887c8c89459d2bbe6a0fff0339
ntoric <ntoric.com@gmail.com>
2026-07-01 00:56:50 +0530

    Live

--- mobile_app/lib/services/api_service.dart
+++ mobile_app/lib/services/api_service.dart
@@ -4,7 +4,7 @@ import 'package:shared_preferences/shared_preferences.dart';
 import '../models/models.dart';
 
 class ApiService {
-  static const String baseUrl = 'http://10.0.2.2:4000/api';
+  static const String baseUrl = 'https://gigit.ntoric.com/api';
   static const String _tokenKey = 'auth_token';
 
   static String? _cachedToken;
